db35542dc2ff9d08a330e02601eca5d6e66d6b26
Henry <henry1192_2004@live.com.au>
2025-01-05 17:08:41 +1100

    added skeleton code and folder

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('FlexPay API is running...');
+});
+
+export default app;
