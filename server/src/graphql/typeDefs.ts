c0fb53ed978c86d75a64f5c56ca558e571e69e14
Henry <henry1192_2004@live.com.au>
2025-01-25 17:46:08 +1100

    removed REST and replace with graphQL

---
+++ server/src/graphql/typeDefs.ts
@@ -0,0 +1,47 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    walletBalance: Float!
+    walletLocked: Boolean!
+    transactions: [Transaction]
+  }
+
+  type Wallet {
+    id: ID!
+    balance: Float!
+    transactions: [Transaction]
+  }
+
+  type Transaction {
+    id: ID!
+    amount: Float!
+    type: String!
+    date: String!
+  }
+
+  type AuthPayload {
+    user: User
+    token: String!
+  }
+
+  type Query {
+    getUser(id: ID!): User
+    getTransactions(userId: ID!, limit: Int, offset: Int): [Transaction]
+  }
+
+  type Mutation {
+    register(username: String!, email: String!, password: String!): AuthPayload
+    login(email: String!, password: String!): AuthPayload
+    addFunds(userId: ID!, amount: Float!): Wallet
+    withdrawFunds(userId: ID!, amount: Float!): Wallet
+    transferFunds(senderId: ID!, receiverId: ID!, amount: Float!): Wallet
+    lockWallet(userId: ID!): User
+    unlockWallet(userId: ID!): User
+  }
+`;
+
+export default typeDefs;
