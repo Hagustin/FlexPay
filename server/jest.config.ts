b6835eaea125b682151ebfc8cef51853061c9ec1
Henry <henry1192_2004@live.com.au>
2025-01-25 18:31:33 +1100

    Added GitHub Actions workflow for Jest tests

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,10 +1,8 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
   testMatch: ["**/__tests__/**/*.test.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json"
-    }
-  }
 };
