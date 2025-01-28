4ff2183b8c4796bdb6e2e3366a8e9f013171edf7
shareefevans <shareef3vans@gmail.com>
2025-01-28 20:03:35 +1100

    installing tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import './App.css';
+// import { useState } from 'react';
 import Login from './components/signIn';
 import SignUp from './components/signUp';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <p className="font-semibold text-cyan-500">test</p>
       <SignUp />
       <Login />
     </div>
